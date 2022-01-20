5429946f99be46f104f2aead3e55429ad2028969
EduardoDahmer <eduardofdahmer@hotmail.com>
2022-01-20 10:53:36 -0300

    Commit 1 com tela de login

---
+++ TelaLogin/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TelaLogin
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string UsuarioSistema = "Usuário";
+            string Senha = "123";
+
+            Console.Write("Usuário: ");
+            string UsuarioInserido = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Senha: ");
+            string SenhaInserida = Console.ReadLine();
+            Console.WriteLine();
+
+            if(UsuarioInserido != UsuarioSistema || SenhaInserida != Senha)
+            {
+                Console.WriteLine("Usuário ou senha inválidos");
+                Console.WriteLine();
+            }
+            if(UsuarioInserido == UsuarioSistema && SenhaInserida == Senha)
+            {
+                Console.WriteLine("Acesso Liberado");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
